3ddbc1a705be7ebb8059f210bb46287cd2388391
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-04-03 20:47:42 -0700

    Add missing use statement for ConsoleOutputInterface.

--- src/CommandProcessor.php
+++ src/CommandProcessor.php
@@ -4,6 +4,7 @@ namespace Consolidation\AnnotationCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
 
 use Consolidation\Formatters\FormatterManager;
 
