588d664eab290530bede52e71eaae34bf2a40788
Frol Kryuchkov <frol.kruchkov@gmail.com>
2017-02-09 23:43:48 +0300

    default value type check

--- src/Parser/CommandInfo.php
+++ src/Parser/CommandInfo.php
@@ -581,7 +581,7 @@ class CommandInfo
                 $explicitOptions[$fullName] = new InputOption($fullName, $shortcut, InputOption::VALUE_NONE, $description);
             } elseif ($defaultValue === InputOption::VALUE_REQUIRED) {
                 $explicitOptions[$fullName] = new InputOption($fullName, $shortcut, InputOption::VALUE_REQUIRED, $description);
-            } elseif ($defaultValue === InputOption::VALUE_IS_ARRAY) {
+            } elseif (is_array($defaultValue)) {
                 $explicitOptions[$fullName] = new InputOption(
                     $fullName,
                     $shortcut,
