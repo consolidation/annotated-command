46796a8f1d6b01c4f2db3be3c239a4b137665ddc
peter279k <peter279k@gmail.com>
2019-11-01 01:13:04 +0800

    Remove unused namespace declaring

--- tests/AnnotatedCommandTest.php
+++ tests/AnnotatedCommandTest.php
@@ -1,9 +1,6 @@
 <?php
 namespace Consolidation\AnnotatedCommand;
 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Application;
