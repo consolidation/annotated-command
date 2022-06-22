dbb6d6a3fc674be5f3c65d17153f2942a5828817
Andy Postnikov <apostnikov@gmail.com>
2022-06-22 17:55:44 +0200

    PHP 8.2 compatibility: dynamic properties are deprecated

--- src/ParameterInjection.php
+++ src/ParameterInjection.php
@@ -7,6 +7,8 @@ namespace Consolidation\AnnotatedCommand;
  */
 class ParameterInjection implements ParameterInjector
 {
+    protected $injectors = [];
+
     public function __construct()
     {
         $this->register('Symfony\Component\Console\Input\InputInterface', $this);
