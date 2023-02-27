1edc9586ec3404774223090242f91c69d6c17d81
Moshe Weitzman <weitzman@tejasa.com>
2023-02-26 22:49:55 -0500

    Allow for just one param to be passed in Help and Topics Attributes (#289)
    * Allow for just hidden to be passed in Help attribute
    
    Drush is not using Attributes for synopsis and description
    
    * Similarly allow passing one part of Topics attribute

--- src/Attributes/Help.php
+++ src/Attributes/Help.php
@@ -13,11 +13,11 @@ class Help
      *   A one line description.
      * @param $synopsis
      *   A multi-line help text.
-     * @param bool|null $hidden
+     * @param bool $hidden
      *   Hide the command from the help list.
      */
     public function __construct(
-        public string $description,
+        public ?string $description = null,
         public ?string $synopsis = null,
         public bool $hidden = false
     ) {
@@ -25,9 +25,13 @@ class Help
 
     public static function handle(\ReflectionAttribute $attribute, CommandInfo $commandInfo)
     {
-        $args = $attribute->getArguments();
-        $commandInfo->setDescription($args['description']);
-        $commandInfo->setHelp(@$args['synopsis']);
-        $commandInfo->setHidden(@$args['hidden']);
+        $instance = $attribute->newInstance();
+        if ($instance->description) {
+            $commandInfo->setDescription($instance->description);
+        }
+        if ($instance->synopsis) {
+            $commandInfo->setHelp($instance->synopsis);
+        }
+        $commandInfo->setHidden($instance->hidden);
     }
 }
