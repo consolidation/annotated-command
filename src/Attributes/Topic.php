b06bff2dd52390d59eaba4c54f7dc55414865778
Moshe Weitzman <weitzman@tejasa.com>
2023-03-04 18:38:59 -0500

    Add Topic Attribute

---
+++ src/Attributes/Topic.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Consolidation\AnnotatedCommand\Attributes;
+
+use Attribute;
+use Consolidation\AnnotatedCommand\Parser\CommandInfo;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class Topic
+{
+    public static function handle(\ReflectionAttribute $attribute, CommandInfo $commandInfo)
+    {
+        $commandInfo->addAnnotation('topic', true);
+    }
+
+}
