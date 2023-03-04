d4420a9ff2fc961ac316b27fb075fccc44b0cf85
Moshe Weitzman <weitzman@tejasa.com>
2023-03-04 10:06:30 -0500

    PHPCS

--- src/Attributes/HookSelector.php
+++ src/Attributes/HookSelector.php
@@ -16,7 +16,7 @@ class HookSelector
      */
     public function __construct(
         public string $name,
-        public ?string $value = NULL,
+        public ?string $value = null,
     ) {
     }
 
