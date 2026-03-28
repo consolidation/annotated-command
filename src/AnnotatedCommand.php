b821fc6f5a3a456db0c555adcbb438f99eade849
Greg Anderson <greg@pantheon.io>
2026-03-28 16:58:20 -0700

    Add : void return type to interact()

--- src/AnnotatedCommand.php
+++ src/AnnotatedCommand.php
@@ -358,7 +358,7 @@ class AnnotatedCommand extends Command implements HelpDocumentAlter
     /**
      * {@inheritdoc}
      */
-    protected function interact(InputInterface $input, OutputInterface $output)
+    protected function interact(InputInterface $input, OutputInterface $output): void
     {
         $state = $this->injectIntoCommandfileInstance($input, $output);
         $this->commandProcessor()->interact(
