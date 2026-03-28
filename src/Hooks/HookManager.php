de4d581d0dddd45dea0af465cedc93bbe4862165
Andy Postnikov <apostnikov@gmail.com>
2026-03-28 22:33:54 +0100

    Allow Symfony 8 (#322)
    Ref: https://github.com/drush-ops/drush/issues/6536

--- src/Hooks/HookManager.php
+++ src/Hooks/HookManager.php
@@ -427,7 +427,7 @@ class HookManager implements EventSubscriberInterface
     /**
      * @{@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [ConsoleEvents::COMMAND => 'callCommandEventHooks'];
     }
